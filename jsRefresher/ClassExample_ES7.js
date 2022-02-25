0bd8a129b4ccbb51eb6d0286949132a3b3cfea21
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-02-25 15:11:58 -0500

    class example es6 vs es7

--- jsRefresher/ClassExample_ES7.js
+++ jsRefresher/ClassExample_ES7.js
@@ -1,19 +1,15 @@
 class Human {
-    constructor() {
-      this.gender = "male";
-    }
-    printGender() {
+    gender = "male";
+    printGender =  () => {
       console.log(this.gender);
     }
   }
   class Person extends Human {
     // simple way to add properties is using constructor
-    constructor() {
-      //when extends class, must call super constuctor in derived class. super() needed.
-      super();
-      this.name = "Max";
-    }
-    printMyName() {
+      //becuase super class Human does not have constructor anymore, super() is not needed.
+      name = "Max";
+      gender = 'female';
+    printMyName = () => {
       console.log(this.name);
     }
   }
