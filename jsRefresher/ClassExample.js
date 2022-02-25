9cc5003413fd3b4627001383373dd22f21679a01
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-02-25 12:48:36 -0500

    class extends example

--- jsRefresher/ClassExample.js
+++ jsRefresher/ClassExample.js
@@ -1,13 +1,24 @@
-class Person {
-    // simple way to add properties is using constructor
-    constructor() {
-        this.name = 'Max';
-    }
-    printMyName() {
-        console.log(this.name);
-    }
+class Human {
+  constructor() {
+    this.gender = "male";
+  }
+  printGender() {
+    console.log(this.gender);
+  }
+}
+class Person extends Human {
+  // simple way to add properties is using constructor
+  constructor() {
+    //when extends class, must call super constuctor in derived class. super() needed.
+    super();
+    this.name = "Max";
+  }
+  printMyName() {
+    console.log(this.name);
+  }
 }
 
 // Instantiate class and store in constant
 const person = new Person();
 person.printMyName();
+person.printGender();
