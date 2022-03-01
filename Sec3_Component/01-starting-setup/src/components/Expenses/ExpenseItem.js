b32491ab6500a84814994ab0569f8edeaff46217
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-03-01 13:46:36 -0500

    useState() demo

--- Sec3_Component/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ Sec3_Component/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import React, {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
+  // useState() should be inside component
+  const [title, setTitle] = useState(props.title); //array destructuring
+
   const clickHandler = () => {
-    console.log('clicked!!')
+    setTitle('Updated!');
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button> 
