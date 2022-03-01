fb1c50cb9c7f900b80fc606c71e8b5eeb54a8eaa
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-03-01 12:46:34 -0500

    changed to arrow function

--- Sec3_Component/01-starting-setup/src/components/UI/Card.js
+++ Sec3_Component/01-starting-setup/src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import "./Card.css"
 
-function Card(props) {
+const Card = (props) => {
     const classes = 'card '+ props.className; // css multiple classes, reference: https://css-tricks.com/multiple-class-id-selectors/ 
     return <div className={classes}>{props.children}</div>
 }
