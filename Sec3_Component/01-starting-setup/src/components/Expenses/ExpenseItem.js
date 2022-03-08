3e5e13dbb2cb321772ae3e8d7addbe060a33db99
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-03-08 01:35:15 -0500

    used stateful lists(dynamic expense list)

--- Sec3_Component/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ Sec3_Component/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
-  // useState() should be inside component
-  const [title, setTitle] = useState(props.title); //array destructuring
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button> 
-       {/* we don't execute function with (). clickHandler() x
-       becuase if I add (), js would execute this when this line of code is being parsed. */}
     </Card>
   );
 }
--- Sec3_Component/01-starting-setup/src/components/Expenses/Expenses.js
+++ Sec3_Component/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,42 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card'
-import ExpensesFilter from "./ExpensesFilter"
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const onSaveFilterYearHandler = (enteredYear) => {
     setFilteredYear(enteredYear);
-  }
+  };
   return (
-  <div>
-  <Card className="expenses">
-  <ExpensesFilter selected={filteredYear} onSaveYear={onSaveFilterYearHandler}/>
-    <ExpenseItem
-      title={props.expenses[0].title}
-      amount={props.expenses[0].amount}
-      date={props.expenses[0].date}
-    />
-    <ExpenseItem
-      title={props.expenses[1].title}
-      amount={props.expenses[1].amount}
-      date={props.expenses[1].date}
-    />
-    <ExpenseItem
-      title={props.expenses[2].title}
-      amount={props.expenses[2].amount}
-      date={props.expenses[2].date}
-    />
-    <ExpenseItem
-      title={props.expenses[3].title}
-      amount={props.expenses[3].amount}
-      date={props.expenses[3].date}
-    />
-  </Card>
-  </div>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onSaveYear={onSaveFilterYearHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
